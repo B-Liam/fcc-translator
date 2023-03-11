a909764c4c201d169b1e7d491d1764c098960ba6
B-Liam <liam@oblbr.co.uk>
2023-03-11 10:01:49 +0000

    first commit

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,18 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    usToGB (string) {
+
+        return "this is the text translated to British English"
+
+    };
+
+    gbToUS (string) {
+
+        return "this is the text translated to US English"
+
+    }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,57 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+
+      let language = req.body.locale
+      let string = req.body.text
+
+      console.log({
+        locale: language,
+        text: string
+      })
+
+      //Reject missing text string
+      if ( string == "" ) {
+        res.json({
+          error: 'No text to translate'
+        })
+        return
+      }
+
+      //Reject missing strings
+      if ( language == undefined || string == undefined  ) {
+          res.json({
+            error: 'Required field(s) missing'
+          })
+          return
+      }
+
+
+      //Reject wrong locale
+      if ( language != "american-to-british" || language != "british-to-american" ) {
+        res.json({
+          error: 'Invalid value for locale field'
+        })
+      }
+
+      //Create a shared translate variable
+      //To store the answer in
+      let translate;
+
+      //Call the appropriate translate function
+      //Based on the locale provided
+      if ( language == "american-to-british" ) {
+        translate = translator.usToGB(string);
+      }
+
+      if ( language == "british-to-american" ) {
+        translate = translator.gbToUS(string);
+      }
+
+      res.send({
+        text: string,
+        translation: translate
+      })
       
     });
 };
